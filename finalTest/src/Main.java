a3f395d85a8ca0446db7bdb5fda1931e8e5db102
xcuong-512 <xcuong286@gmail.com>
2026-03-20 09:28:38 +0700

    Submit

---
+++ finalTest/src/Main.java
@@ -0,0 +1,8 @@
+import controller.ProductController;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductController controller = new ProductController();
+        controller.menu();
+    }
+}
