ecd034e0d1b390c82237850145266c1ccf77c64a
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-05-18 09:19:41 +0700

    add file

--- Question-1/BankSystem/obj/Debug/net8.0/BankSystem.AssemblyInfo.cs
+++ Question-1/BankSystem/obj/Debug/net8.0/BankSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0113a8d15f54d5cf68530dab88c20bab04224464")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
